7f627d6e2bffbc4bea4f9725f56832605b070033
Sarmadkubba <83927120+Sarmadkubba@users.noreply.github.com>
2021-05-09 18:23:23 -0700

    Python file

---
+++ Python Assignment.py
@@ -0,0 +1,19 @@
+number_grade = input("Enter your grade here")
+
+letter_grade = "F"
+
+# your code here
+def calc_average(total):
+    return total/4
+
+def determine_letter_grade(grade):
+    if 90 <= grade <= 100:
+         A
+    elif 80 <= grade <= 89:
+         B
+    elif 70 <= grade <= 79:
+         C
+    elif 60 <= grade <= 69:
+         D
+    else:
+         F
