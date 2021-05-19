4461c2937a06ef31b2aa5d388e866301c7fcbe6a
Sarmadkubba <83927120+Sarmadkubba@users.noreply.github.com>
2021-05-18 20:29:21 -0700

    Update Python Assignment.py

--- Python Assignment.py
+++ Python Assignment.py
@@ -1,19 +1,8 @@
-number_grade = input("Enter your grade here")
+def lone_sum(a, b, c):    if a >= b:        
+        return c    elif a == c:        
+        return b    elif b == c:        
+        return a    elif a == b and a == c and b == c:        
+        return 0   
+    else:        return a+b+c
 
-letter_grade = "F"
-
-# your code here
-def calc_average(total):
-    return total/4
-
-def determine_letter_grade(grade):
-    if 90 <= grade <= 100:
-         A
-    elif 80 <= grade <= 89:
-         B
-    elif 70 <= grade <= 79:
-         C
-    elif 60 <= grade <= 69:
-         D
-    else:
-         F
+    
